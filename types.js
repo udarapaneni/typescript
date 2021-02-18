b48fa13cdedda373417bad91eb28b47055bdc29f
Umakanth Darapaneni <umakanth158@gmail.com>
2021-02-18 15:27:39 +0530

    Typescript variable types and type annotations

---
+++ types.js
@@ -0,0 +1,16 @@
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, 'a', true];
+var colorRed = 0;
+var colorGreen = 1;
+var colorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var backgroundColor = Color.Blue;
---
+++ types.ts
@@ -0,0 +1,18 @@
+let a: number;
+let b: string;
+let c: boolean;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, 'a', true];
+
+const colorRed = 0;
+const colorGreen = 1;
+const colorBlue = 2;
+
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+
+let backgroundColor = Color.Blue;
